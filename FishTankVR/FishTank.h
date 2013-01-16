65ce3c7b9cdbd713fd9771b8c574d1434991f317
eaa3 <eaa3>
2013-01-16 17:40:15 -0200

    Updated data strutures, introduced methods setView and setFrustum

--- FishTankVR/FishTank.h
+++ FishTankVR/FishTank.h
@@ -1,14 +1,23 @@
 #pragma once
+
+#include "Matrices.h"
+#include "Vectors.h"
+
 class FishTank
 {
 public:
 
-	float proj[16];
-	float rt[16];
+	Matrix4 proj;
+	Matrix4 rt;
 
 	float l, r, b, t, n, f;
 
 	FishTank(float l, float r, float b, float t, float n, float f);
+
+	void setFrustum( Vector3 eyepos );
+	void setView( Vector3 eyepos );
+
+
 	~FishTank(void);
 };
 
--- FishTankVR/tld/tld_util.h
+++ FishTankVR/tld/tld_util.h
@@ -7,7 +7,6 @@
 #include <gl/GLU.h>
 #include <gl/GL.h>
 #include <glut.h>
-#include "Vector3.h"
 
 using tld::BoundingBox;
 
@@ -21,33 +20,6 @@ bool tldSortByOverlapDesc(pair<int,float> bb1 , pair<int,float> bb2);
 Mat get_roi(Mat& src, BoundingBox box, int padding = 0);
 void draw_box(BoundingBox& b, Mat& src, Scalar color);
 
-struct Color3f
-{
-    //0->R,1->G,2->B
-    GLfloat color[3];
-
-    Color3f();
-
-    Color3f(GLfloat r, GLfloat g, GLfloat b);
-
-
-    operator GLfloat*();
-
-};
-
-struct Cube
-{
-    GLfloat edge;
-    Vector3 pos;
-    GLfloat angle[3];
-    Color3f color[6];
-
-    Cube(GLfloat edge = 0.0f);
-
-    GLfloat& operator[](int i);
-
-    void draw();
-};
 
 
 #endif
