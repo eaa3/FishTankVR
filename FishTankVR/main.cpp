a7bdb0b544f4d65aacbc5d58178039d4943b8db9
eaa3 <eaa3>
2013-01-25 14:56:53 -0200

    loading textures

--- FishTankVR/main.cpp
+++ FishTankVR/main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include "HeadTracker.h"
 #include "FishTank.h"
+//#include <GL/glext.h>
 
 using namespace cv;
 using namespace tld;
@@ -20,10 +21,38 @@ float realW = 57.61f;
 float realH = 37.5f;
 float angle = 0;
 
+GLuint tex;
+
 float estimator(const BoundingBox& bb){
 
 	return -0.3756*bb.w + 126.73;
 }
+
+GLuint loadTexture(string filename)
+{
+	Mat img = imread(filename);
+
+	GLuint texture_id;
+
+	
+	glGenTextures(1, &texture_id);
+
+	glBindTexture(GL_TEXTURE_2D, texture_id);
+
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    // Edit the texture object's image data using the information SDL_Surface gives us
+
+	
+    //glTexImage2D( GL_TEXTURE_2D, 0, 4, img.cols, img.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data );
+
+	//img.release();
+
+	
+	return texture_id;
+}
+
 
 void init()
 {
@@ -41,6 +70,10 @@ void init()
 
 	glEnable(GL_DEPTH_TEST);
 
+
+	tex = loadTexture("window.png");
+
+
 }
 
 void drawQuad(float angle, Vec3f t, Vec3f rot_axis, Vec4f color = Vec4f(0.8f,0.8f,0.8f,0.5f))
@@ -48,20 +81,55 @@ void drawQuad(float angle, Vec3f t, Vec3f rot_axis, Vec4f color = Vec4f(0.8f,0.8
 	glPushMatrix();
 	glColor4f(color[0],color[1], color[2], color[3]);
 	glTranslatef(t[0], t[1], t[2]);
-	glRotatef(angle, rot_axis[0],rot_axis[1],rot_axis[2]);	
+	glRotatef(angle, rot_axis[0],rot_axis[1],rot_axis[2]);
+
+
+	//glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, tex);
+
 	glBegin(GL_QUADS);
 	{
+		//glTexCoord2i(0,0);
 		glVertex3f(ft.l, ft.b, 0);
+		//glTexCoord2i(1,0);
 		glVertex3f(ft.r, ft.b, 0);
+		//glTexCoord2i(1,1);
 		glVertex3f(ft.r, ft.t, 0);
+		//glTexCoord2i(0,1);
 		glVertex3f(ft.l, ft.t, 0);
 
 	}
 	glEnd();
+
+
+	glDisable(GL_TEXTURE_2D);
+
 	glPopMatrix();
 
 }
+
+void drawCube(float size, Vec3f pos, Vec4f color, bool wire_form = false)
+{
+
+	glPushMatrix();
+
+	glTranslatef(pos[0], pos[1], pos[2]);
+
+	glColor4f(color[0], color[1], color[2], color[3]);
 	
+	wire_form? glutWireCube(10.2f) : glutSolidCube(size);
+
+	glPopMatrix();	
+
+}
+
+void drawBorderedCube(float size, Vec3f pos, Vec4f color, Vec4f border_color)
+{
+	drawCube(size, pos, color, false);
+	drawCube(size+0.01, pos, border_color, true);
+}
+
+
 
 
 
@@ -93,17 +161,17 @@ void display()
 	ft.setFrustum( eyepos );
 
 	
-	//glFrustum(ft.l,ft.r,ft.b,ft.t, ft.n, ft.f);
+	//glFrustum((ft.l - eyepos.x)/eyepos.z,(ft.r - eyepos.x)/eyepos.z,(ft.b - eyepos.y)/eyepos.z,(ft.t - eyepos.y)/eyepos.z, ft.n, ft.f);
 	
 	glMultMatrixf(ft.proj.getTranspose());
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glEnable(GL_BLEND);	
 
-	//ft.setView( eyepos );
-	//glMultMatrixf( ft.rt.getTranspose() );
-	gluLookAt(-eyepos.x,eyepos.y,-eyepos.z,eyepos.x,eyepos.y,0,0,1,0);
+
+	ft.setView( eyepos );
+	glMultMatrixf( ft.rt.getTranspose() );
+	
 
 	
 	//drawQuad(90, Vec3f(ft.r,0,0), Vec3f(0,0,1), Vec4f(0.8,0.2,0.2,0.1)); //right quad
@@ -124,24 +192,13 @@ void display()
 	cout << "\r" << "eyepos = " << eyepos << "\nModelView_A\n" << modelview << "\nModelview_B" << "--------------------\n" << ft.rt.transpose() << "--------------------\n";
 
 	
-	
-	glTranslatef(5,-5, -20);
-	//angle = (int(angle)+1)%360;
-	
-	glColor3f(0.7,0.7,0.7);
-	
-	glutSolidCube(10.0f);
-	glColor3f(0,0,0);
-	glutWireCube(10.2f);
 
-	glTranslatef(-10,10, -10);
-	//angle = (int(angle)+1)%360;
+	drawBorderedCube(10.0f, Vec3f(0,0,20), Vec4f(0.7,0.7,0.7,0.1), Vec4f(0,0,0,0));
 	
-	glColor3f(0.7,0.7,0.7);
+	drawBorderedCube(10.0f, Vec3f(-10,10, -20), Vec4f(0.7,0.7,0.7,0.1), Vec4f(0,0,0,0) );
+
+	drawBorderedCube(10.0f, Vec3f(30,10, -20), Vec4f(0.3,0.3,0.8,0.3), Vec4f(0,0,0,0) );
 	
-	glutSolidCube(10.0f);
-	glColor3f(0,0,0);
-	glutWireCube(10.2f);
 
 	glutSwapBuffers();
 	glutPostRedisplay();
@@ -175,7 +232,6 @@ int main(int argc, char** argv)
 	glutIdleFunc(idleFunc);	
 
 	//atexit(deinit);
-
 	glutMainLoop();
 
 	
